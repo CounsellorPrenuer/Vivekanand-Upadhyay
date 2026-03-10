a4b94a1a5663bc57c6a59269314e40248ce5ac01
Gaurav <your-email@gmail.com>
2026-03-11 01:13:22 +0530

    Disable CDN for immediate Sanity data visibility

--- client/src/lib/sanity.ts
+++ client/src/lib/sanity.ts
@@ -4,7 +4,7 @@ import imageUrlBuilder from "@sanity/image-url";
 export const client = createClient({
   projectId: "scuuz9jw",
   dataset: "production",
-  useCdn: true,
+  useCdn: false,
   apiVersion: "2024-03-10",
   token: "skv67jYYnd1SYMqdsDgf0j9ixTlG4b3KcybBSe0tqXM2QQbuOl3ufk13alY1dMc8lGPQ2eQQKY8IV7Nh6GOUJ64bufbAYNkS1DnvPiEFJU1VZJxa6uUcCpp35YJw0mk1OpcL9Zw8HxYl43RzWBRsVmxoVcIqCOIxQW4EWFMQuAGBuaYnEZed",
 });
